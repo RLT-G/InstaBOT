3d07b0182a415c18a79e18ec4302ee2d8ddebb2e
konstantin <inco.k.b.blizz@gmail.com>
2023-07-13 07:19:45 +0300

    Предфиналочка. Необходим тест

--- insta_bot.py
+++ insta_bot.py
@@ -1,15 +1,18 @@
 __author__ = 'Konstantin Bychkov <inco.k.b.blizz@gmail.com>'
 __copyright__ = 'Copyright 2023, RLT IP.'
-__version__ = '0.1.7'
+__version__ = '0.2.8'
 
 
-import json
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import selenium.webdriver.chrome.options
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+
+from multiprocessing import Process, Queue
+from random import randint, choice
 from time import sleep
-from random import randint
+import datetime
+import json
 import sys
 
 
@@ -83,6 +86,12 @@ class ManagerBase:
     def phase_2(self):
         ...
 
+    def launch_detector(self):
+        ...
+
+    def update_detector(self):
+        ...
+
     def all_c(self):
         print(f"all commands:\n{self.command.keys()}")
 
@@ -142,8 +151,47 @@ class Manager(ManagerBase):
         super().__init__()
         self.scripts = Scripts()
         self.words = self.use_json("d_data/comments.json")["comments"]
+        self.users_data = self.use_json("d_data/users.json")
+        self.bots_data = self.use_json("d_data/bots.json")
+        self.input_time = [
+            self.scripts.rtime_from_range("07:00-9:00"), 
+            self.scripts.rtime_from_range("20:00-22:00")
+            ]
 
     def phase_1(self, username: str, password: str, link: str) -> None:
+        def like() -> None:
+            try:
+                like_button = browser.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div[1]/div/div[2]/div/div[3]/div[1]/div[1]/span[1]/div/div')
+                like_button.click()
+            except Exception as ex:
+                print(f"Like\n{ex}")
+            self.scripts.have_a_rest(1)
+        def comment() -> None:
+            try:
+                smile = browser.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div[1]/div/div[2]/div/section/div/form/div/div[1]/div/div')
+                sleep(5)
+                smile.click()
+                sleep(5)
+                smile.click()
+                sleep(5)
+                comment_area = browser.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div[1]/div/div[2]/div/section/div/form/div/textarea')
+                sleep(5)
+                comment_area.send_keys(self.words[randint(0, len(self.words) - 1)])
+                sleep(5)
+                confirm_button = browser.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div[1]/div/div[2]/div/section/div/form/div/div[2]/div')
+                sleep(5)
+                confirm_button.click()
+                sleep(5)
+            except Exception as ex:
+                print(f"comment\n{ex}")
+            self.scripts.have_a_rest(1)
+        def saves() -> None:
+            try:
+                save_button = browser.find_element(By.XPATH,'/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div[1]/div/div[2]/div/div[3]/div[1]/div[3]/div/div/div/div')
+                save_button.click()
+            except Exception as ex:
+                print(f"Save\n{ex}")
+            self.scripts.have_a_rest(1)
         # connect to browser
         try:
             options = selenium.webdriver.chrome.options.ChromiumOptions()
@@ -193,7 +241,7 @@ class Manager(ManagerBase):
             self.scripts.have_a_rest(3)
             hrefs = browser.find_elements(By.TAG_NAME, 'a')
             current_links = [item.get_attribute('href') for item in hrefs if '/p/' in item.get_attribute('href')]
-            visited, max_visited = 0, randint(12, 20)
+            visited, max_visited = 0, randint(7, 12)
             for l in current_links:
                 if visited >= max_visited:
                     break
@@ -210,52 +258,15 @@ class Manager(ManagerBase):
                 except Exception as ex:
                     print(f"Sound\n{ex}")
                 finally:
-                    # try click on the like
-                    if True:#randint(1, 10) in range(1, 5):
-                        try:
-                            like_button = browser.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div[1]/div/div[2]/div/div[3]/div[1]/div[1]/span[1]/div/div')
-                            like_button.click()
-                        except Exception as ex:
-                            print(f"Like\n{ex}")
-                        self.scripts.have_a_rest(1)
-                        # try send comment
-                        if True:#randint(1, 10) in range(1, 3):
-                            try:
-
-                                smile = browser.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div[1]/div/div[2]/div/section/div/form/div/div[1]/div/div')
-                                # self.scripts.have_a_rest(1)
-                                sleep(5)
-                                smile.click()
-                                # self.scripts.have_a_rest(2)
-                                sleep(5)
-                                smile.click()
-                                # self.scripts.have_a_rest(1)
-                                sleep(5)
-                                comment_area = browser.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div[1]/div/div[2]/div/section/div/form/div/textarea')
-                                # self.scripts.have_a_rest(1)
-                                sleep(5)
-                                comment_area.send_keys(self.words[randint(0, len(self.words) - 1)])
-                                # self.scripts.have_a_rest(2)
-                                sleep(5)
-                                confirm_button = browser.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div[1]/div/div[2]/div/section/div/form/div/div[2]/div')
-                                # self.scripts.have_a_rest(1)
-                                sleep(5)
-                                confirm_button.click()
-                                sleep(5)
-                                # self.scripts.have_a_rest(2)
-
-                            except Exception as ex:
-                                print(f"comment\n{ex}")
-                            self.scripts.have_a_rest(2)
-                            
-                            # try click on save button
-                            if True:#randint(1, 10) == 1:
-                                try:
-                                    save_button = browser.find_element(By.XPATH,'/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div[1]/div/div[2]/div/div[3]/div[1]/div[3]/div/div/div/div')
-                                    save_button.click()
-                                except Exception as ex:
-                                    print(f"Save\n{ex}")
-                    self.scripts.have_a_rest(2)
+                    r_number = randint(1, 100)
+                    if r_number in range(1, 41): 
+                        ...
+                    elif r_number in range(41, 76): 
+                        like()
+                    elif r_number in range(76, 91): 
+                        like(), comment()
+                    else: 
+                        like(), comment(), saves()
         try:
             # try subscribe
             subscribe_button = browser.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div[1]/div/div[2]/div/div[1]/header/div[2]/div[1]/div[2]/button/div/div')
@@ -267,10 +278,77 @@ class Manager(ManagerBase):
         browser.quit()
 
     def phase_2(self):
+        # в первый день 50% ботов работают В 2й 30% в 3й 20%
+        # В течение дня по 4 временным рамкам порвну запускаем ботов на лайки
+
+        # Пишем метод на тупо лайк с коментом и прочей хуйней
+        # выплываем из говна и радуемся
         ...
 
     def start(self):
-        self.phase_1(username="+79280113665", password="Potriot13041974!", link="https://www.instagram.com/crescentt/")
+        # Если и ебанет то только тут
+        # self.phase_1(username="+79280113665", password="Potriot13041974!", link="https://www.instagram.com/kimfelix143/")
+        while True:
+            queue = Queue()
+            if datetime.datetime.now().strftime("%H:%M") == "00:00":
+                self.input_time = [
+                self.scripts.rtime_from_range("07:00-9:00"), 
+                self.scripts.rtime_from_range("20:00-22:00")
+                ]
+
+            launch = Process(target=self.launch_detector, args=(queue, ), daemon=False)
+            update = Process(target=self.update_detector, daemon=False)
+
+            launch.start()
+            update.start()
+
+            launch.join()
+            update_data = queue.get()
+            if update_data is not None:
+                self.users_data
+                for key, value in self.users_data.items():
+                    if key in list(update_data.keys()):
+                        self.users_data[key]['signed_bots'].append(update_data[key])
+                with open("d_data/users.json", 'w') as users_data:
+                    json.dump(self.users_data, users_data)
+
+            update.join()
+
+    # Уважаемый примат читающий это завтра - не забудь try catch везде проставить. Процессы же ультанут - пезда бд будет
+
+    def launch_detector(self, queue):
+        # Ну или тут
+        current_time = datetime.datetime.now().strftime("%H:%M")
+        if current_time == self.input_time[0] or current_time == self.input_time[1]:
+            update_data = {}
+            bots_name = list(self.bots_data.keys())
+            defs = []
+            for key, value in self.users_data.items():
+                bot_name = self.scripts.get_random_unique_element(bots_name, value['signed_bots'])
+                if bot_name is not None:
+                    username = self.bots_data[bot_name]['username']
+                    password = self.bots_data[bot_name]['password']
+                    link = value['main_links']
+                    process = Process(target=self.phase_1, args=(username, password, link), daemon=False)
+                    update_data[key] = bot_name
+                    defs.append(process)
+            if len(defs) != 0:
+                for process in defs:
+                    process.start()
+                for process in defs:
+                    process.join()
+            if update_data != {}:
+                queue.put(update_data)
+            else:
+                queue.put(None)
+        else:
+            queue.put(None)
+
+    def update_detector(self):
+        # Пробегаем по человечкам
+        # Сверяем 
+        # если что запускаем фазу 2
+        ...
         
     def exit(self):
         ...
@@ -279,6 +357,29 @@ class Manager(ManagerBase):
 class Scripts:
     def __init__(self):
         ...
+
+    @staticmethod
+    def get_random_unique_element(list1, list2):
+        """Возвращает случайный элемент из списка 1, который отсутствует в списке 2."""
+        unique_elements = list(set(list1) - set(list2))
+        if unique_elements:
+            return choice(unique_elements)
+        else:
+            return None
+        
+    @staticmethod
+    def rtime_from_range(rang: str = "00:00-23:00") -> list | None:
+        def convert_to_sec(value: str) -> int:
+            value = list(map(int, value.split(":")))
+            value = value[0] * 60 + value[1]
+            return value
+        def convert_to_time(value: int) -> str:
+            div = value // 60
+            mod = value % 60
+            return f"{div if len(str(div)) == 2 else '0' + str(div)}:{mod if len(str(mod)) == 2 else '0' + str(mod)}"
+        rang = list(map(convert_to_sec, rang.split("-")))
+        return convert_to_time(randint(rang[0], rang[1]))
+
 
     @staticmethod
     def have_a_rest(mode: int) -> None:
@@ -294,7 +395,7 @@ class Scripts:
             """
         match mode:
             case 1:
-                sleep(randint(1, 5))
+                sleep(randint(2, 5))
             case 2:
                 sleep(randint(5, 10))
             case 3:
